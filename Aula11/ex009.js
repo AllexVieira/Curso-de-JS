c8bf403521c74a01038d2b8a32f114cba0d07fc7
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-17 22:29:43 -0300

    novos ex

---
+++ Aula11/ex009.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h por hora !`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida de 60km/h. MULTADO`)
+}
+console.log(`Dirija com cuidado e sempre use sinto de segurança.`)
