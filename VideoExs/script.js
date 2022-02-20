d93b8374a5774b8d8e2db2f774fe4512943ed57e
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-19 21:30:20 -0300

    algumas mudanças

---
+++ VideoExs/script.js
@@ -0,0 +1,15 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var sec = data.getSeconds()
+    msg.innerHTML = `A hora agora é <strong>${hora}:${min}:${sec}</strong>`
+    if (hora >= 6 && hora <= 12) {
+        
+    } else if {
+        
+    } else if {
+
+    }
