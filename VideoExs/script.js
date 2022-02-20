193494e54562cadf365819566037ad17d9851147
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-19 22:11:35 -0300

    Update script.js

--- VideoExs/script.js
+++ VideoExs/script.js
@@ -5,12 +5,20 @@ function carregar() {
     var hora = data.getHours()
     var min = data.getMinutes()
     var sec = data.getSeconds()
-    msg.innerHTML = `A hora agora é <strong>${hora}:${min}:${sec}</strong>`
-    if (hora >= 6 && hora <= 12) {
-        
-    } else if() {
-        
-    } else {
 
+    //msg.innerHTML = `A hora agora é <strong>${hora}:${min}:${sec}</strong></p>`
+
+    if (hora >= 6 && hora <= 11) {
+        msg.innerHTML = `BOM DIA !!!<p>A hora agora é <strong>${hora}:${min}:${sec}</strong>`
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if(hora >= 12 && hora <=17) {
+        msg.innerHTML = `BOA TARDE !!!<p>A hora agora é <strong>${hora}:${min}:${sec}</strong>`
+        img.src ='fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        msg.innerHTML = `BOA NOITE !!!<p>A hora agora é <strong>${hora}:${min}:${sec}</strong>`
+        img.src ='fotonoite.png'
+        document.body.style.background = '#515154'
     }
 }
