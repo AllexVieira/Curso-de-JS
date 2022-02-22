97c9b434efee8fb8c26d26e626229ecefcc115ca
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-22 20:31:30 -0300

    final

--- VideoExs3/script.js
+++ VideoExs3/script.js
@@ -11,9 +11,13 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - fano.value
         var genero = ''
-
+        //criando a imagem
         var img = document.createElement('img')
         img.setAttribute('id','foto')
+        img.setAttribute('style', 'width:100%; height:400px;')
+        //para ajustar as configuraçoes da imagem o comando acima com style pode mudar tudo de uma vez ou pode ser feito individualmente como nos comandos abaixo.
+        //img.setAttribute('width', '50%')
+        //img.setAttribute('height', '400px')
 
         if (fsex[0].checked) {
             genero = 'Homem'
@@ -22,26 +26,33 @@ function verificar() {
                 img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade < 22 ){
                 //jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 60 ){
                 //adulto
+                img.setAttribute('src','foto-adulto-m.png')
             } else {
                 //idoso
+                img.setAttribute('src','foto-idoso-m.png')
             }
 
         } else if (fsex[1].checked){
             genero = 'Mulher'
             if (idade >= 0 && idade < 16){
                 //criança
+                img.setAttribute('src','foto-bebe-f.png')
             } else if (idade < 22 ){
                 //jovem
+                img.setAttribute('src','foto-jovem-f.png')
             } else if (idade < 60 ){
                 //adulto
+                img.setAttribute('src','foto-adulto-f.png')
             } else {
                 //idoso
+                img.setAttribute('src','foto-idoso-f.png')
             }
           }
-        res.style.textAlign = 'center' 
-        res.innerHTML = `Detectamos que você é ${genero} com ${idade} anos.` 
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos que você é ${genero} com ${idade} anos.` 
         res.appendChild(img)
      }
 }
