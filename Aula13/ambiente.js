9e9e2010ef782cfa53cfca161f66a38e9f29ae8b
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-23 20:46:16 -0300

    novos exs

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,1 +1,5 @@
-stop
+var a = 1
+    while (a <= 50) {
+        console.log(`Tudo Bem ? ${a}`)
+        a++
+    }
---
+++ Aula13/ambiente2.js
@@ -0,0 +1,6 @@
+var a = 1
+do {
+    console.log(`Bom dia !!! ${a}`)
+    a++
+} while (a <= 10)
+    
