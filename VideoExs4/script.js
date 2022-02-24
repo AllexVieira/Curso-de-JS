e9253f3fe485e3217dca1f66e046b52df760bcd6
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-24 20:24:00 -0300

    Update script.js

--- VideoExs4/script.js
+++ VideoExs4/script.js
@@ -1,20 +1,36 @@
 function contar() {
-    //alert('teste fun ok!')
     var inicio = document.querySelector('input#txtini')
     var fim = document.querySelector('input#txtfim')
     var passo = document.querySelector('input#txtpas') 
     //alert(`${inicio.value},${fim.value},${passo.value}`)
     var res = document.querySelector('div#res')
-    res.innerHTML = ""
-    //alert(`${res.innerHTML}  teste div res`)
-    //var numatu = inicio.value
-    //res.innerHTML = (`${numatu},`)
-    //res.innerHTML = (`${inicio.value}, ${fim.value}, ${passo.value}`)
 
-    for(numatu = inicio.value ;numatu <= fim.value; numatu + passo.value){
-        res.innerHTML = (`${res.innerHTML}` + `,${numatu}, `)
-        numatu = (inicio.value + passo.value)
-        
-    }
-    res.innerHTML = (res.innerHTML + ` FIM !!!`)
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar !'
+        //alert('[ERRO] Faltam dados !')
+
+    } else {
+        res.innerHTML = 'Contando:'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if(i < f){
+            //contagem crescente
+            for(var c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1f449} `
+            }
+        } else {
+            //contagem decrescente
+            for(var c = i; c >= f; c-= p)
+            res.innerHTML += ` ${c} \u{1f449} `
+          } 
+        res.innerHTML += '\u{1f3c1}'
+     }
+
 }
